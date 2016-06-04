803de960e0d97b7a70e63d7a93e9dada79fb6578
Christian Ceciliano <elxris@Iddars-MacBook-Pro.local>
2016-06-04 12:22:01 -0500

    Segundo commit

--- controllers/index.js
+++ controllers/index.js
@@ -1,16 +1,9 @@
 'use strict';
 
-var IndexModel = require('../models/index');
+module.exports = function(router) {
 
-
-module.exports = function (router) {
-
-    var model = new IndexModel();
-
-    router.get('/', function (req, res) {
-        
-        res.send('<code><pre>' + JSON.stringify(model, null, 2) + '</pre></code>');
-        
-    });
+  router.get('/', function(req, res) {
+    res.render('index');
+  });
 
 };
