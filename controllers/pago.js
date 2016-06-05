aeb76bd6c3c50eb2d8d3586c8901757f70c544a7
Rodrigo Moreno <moreno.rdr@gmail.com>
2016-06-04 22:26:37 -0500

    + Primer borrador para la integracion con PayPal

---
+++ controllers/pago.js
@@ -0,0 +1,56 @@
+'use strict';
+
+var paypal = require('paypal-rest-sdk');
+
+module.exports = function(router) {
+
+  router.post('/', function(req, res, next) {
+    var monto = req.body.monto;
+
+    if (!monto) {
+      res.sendStatus(400); // TODO
+    }
+
+    paypal.configure({
+      mode: req.app.kraken.get('paypal:mode'),
+      client_id: req.app.kraken.get('paypal:clientId'),
+      client_secret: req.app.kraken.get('paypal:clientSecret')
+    });
+
+    var paymentObject = {
+      'intent': 'sale',
+      'payer': {
+        'payment_method': 'paypal'
+      },
+      'redirect_urls': {
+        'return_url': 'http://proceso.premiorazondeser.mx/', // TODO
+        'cancel_url': 'http://cineguru.com.mx' // TODO
+      },
+      'transactions': [{
+        'item_list': {
+          'items': [{
+            'name': 'Donativo', //TODO
+            'sku': Date.now().toString(), //TODO
+            'price': monto, //TODO
+            'currency': 'MXN', //TODO
+            'quantity': 1 //TODO
+          }]
+        },
+        'amount': {
+          'currency': 'MXN', //TODO
+          'total': monto //TODO
+        },
+        'description': 'Un donativo para .....' //TODO
+      }]
+    };
+
+    paypal.payment.create(paymentObject, function(err, payment) {
+      if (err) {
+        throw err; //TODO
+      }
+
+      console.log(payment);
+      res.redirect(payment.links[1].href);
+    });
+  });
+};
